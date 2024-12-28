9b8fbc8045a48263587d26cda23ae83882a838cd
Prakhar Sharma <prakhars1406@gmail.com>
2024-12-28 10:03:01 +0000

    playground challenge 2

--- Payroll.sol
+++ Payroll.sol
@@ -31,18 +31,66 @@ contract Payroll {
         _;
     }
 
-    modifier onlyActiveEmployees() {
-        require(isEmployee[msg.sender] && employees[getEmployeeIndex(msg.sender)].isActive, "Only active employees can access this");
-        _;
-    }
+    // modifier onlyActiveEmployees() {
+    //     require(isEmployee[msg.sender] && employees[getEmployeeIndex(msg.sender)].isActive, "Only active employees can access this");
+    //     _;
+    // }
 
     constructor() {
         companyAcc = msg.sender;
         lastPaymentTime = block.timestamp; // Set the initial payment time to now
     }
 
-    // TODO: Add functionality to add a new employee with a salary and check if the employee already exists
+    function addEmployee(uint256 id, uint256 salary, address worker) public onlyCompanyOwner{
+        
+        require(!isEmployee[worker], "Employee with address already exists");
+        isEmployee[worker] = true;
+        
+        Employee memory emp;
+        emp.id = id;
+        emp.worker = worker;
+        emp.salary = salary;
+        emp.timestamp = block.timestamp;
+        emp.isActive = true;
 
+        employees.push(emp);
+        lastPaymentTime = block.timestamp;
+        totalEmployees = totalEmployees+1;
+        totalSalary = totalSalary + salary;
+
+        emit EmployeeAdded(id, worker, salary);
+    }
+
+    function payEmployee() public onlyCompanyOwner {
+
+        require(address(this).balance >= totalSalary, "not enough balance to pay salaries");
+
+        for (uint256 i = 0; i <employees.length; i++) {
+            if (employees[i].isActive) {
+
+                (bool success,) = (employees[i].worker).call{value: employees[i].salary}("");                
+                require(success, "Failes to send salary to employee");
+                companyBal = companyBal - employees[i].salary;
+                lastPaymentTime = block.timestamp;
+                emit Paid(employees[i].id, msg.sender, employees[i].salary, block.timestamp);
+            }
+        }
+
+    }
+
+    // function deactivateEmployee(address worker) public onlyCompanyOwner {
+
+    //     require(isEmployee[worker], "Employee with address does not exists");
+    //     Employees[]
+
+    // }
+
+    // function getEmployeeIndex(address ) view internal returns(uint256){
+        
+
+
+    //     return index
+    // }
     // TODO: Add functionality to deactivate an employee (e.g., termination or leave)
 
     // TODO: Add functionality to check if the payment interval has elapsed since the last payment
@@ -50,6 +98,12 @@ contract Payroll {
     // TODO: Add functionality to process batch payments securely and prevent reentrancy attacks
 
     // TODO: Add functionality to allow the company owner to fund the company balance and track it
+
+    function fundCompany() external  payable {
+        require(msg.value > 0, "fund amount is less than or equal to 0");
+        companyBal = companyBal + msg.value;
+        payable(address(this)).transfer(msg.value);
+    }
 
     // TODO: Add functionality to allow the company owner to update the payment interval dynamically
 
